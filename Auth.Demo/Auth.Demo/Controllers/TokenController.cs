4822e35689e96b39d68706429b9f193ab6cae79c
Alok101 <ijsb1098@gmail.com>
2020-09-02 08:19:45 +0530

    commit3

---
+++ Auth.Demo/Auth.Demo/Controllers/TokenController.cs
@@ -0,0 +1,35 @@
+﻿using Auth.Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Auth.Demo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jwtAuthenticationManager;
+        public TokenController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/Token
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [AllowAnonymous]
+        [HttpPost("Authenticate")]
+        public IActionResult Authenticate([FromBody]UserCred userCred)
+        {
+           var token= jwtAuthenticationManager.Authenticate(userCred.UserName, userCred.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+    }
+}
